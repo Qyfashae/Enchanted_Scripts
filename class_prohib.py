3efe937ddd0875ba7b5ac66e0d0b770d61b17b9a
Felix.T <75949095+Qyfashae@users.noreply.github.com>
2023-09-01 18:21:50 +0100

    class_prohib.py

---
+++ class_prohib.py
@@ -0,0 +1,6 @@
+class Person:
+    def __init__(self) -> None:
+        self._name = "Name"
+
+
+# This example is only to let other programmers know that you should not use the '_name' outside of this class
